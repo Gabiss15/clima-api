1c83acbd13f4e898f68048ede18d3bf84abd80f8
Gabiss15 <raquelgabi.nasci1815@gmail.com>
2022-03-19 12:16:57 -0300

    Adicionando imagens

---
+++ clima-api/teste.js
@@ -0,0 +1,105 @@
+const key = "1efaeca6f9da0c5c936dc24732e21780"
+const input = document.querySelector('input#pesquisa')
+let precipitacao = document.querySelector('h4#humidty')
+let tempMax = document.getElementsByClassName('max-temperatura')
+let tempMin = document.getElementsByClassName('min-temperatura')
+let DayOfWeek = document.querySelector("p#DayOfWeek")
+let sensation = document.querySelector('p#sensation')
+let lat
+let lon
+let cidadePesquisada = document.querySelector('p#cidade')
+
+function request(){
+    let city = input.value
+
+    var dateOfWeek = ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" ]
+    var week = new Date();
+    var number1 = dateOfWeek[week.getDay()]
+
+    var day = new Date()
+    var dia = day.getDate()
+
+    var month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    var mes = new Date();
+    var number2 = month[mes.getMonth()]
+
+    let time = new Date()
+    let hora = time.getHours()
+
+    let moment = ''
+
+    if (hora >= 0 && hora<=11){
+        moment = 'morn'
+    } else if (hora>= 12 && hora <= 18){
+        moment = 'day'
+    } else if (hora > 18 && hora <= 23){
+        moment = 'eve'
+    }
+
+    DayOfWeek.innerHTML = `${number1}, ${dia} de ${number2}`
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${1}&appid=${key}`)
+
+        .then(response => {
+            response.json()
+
+            .then (data =>{
+                lat = data[0]["lat"]
+                lon = data[0]["lon"]
+                cidadePesquisada.innerHTML = `${data[0]["name"]}, ${data[0]["country"]}`
+
+                fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&exclude={part}&appid=${key}`)
+                    .then(response =>{
+                        response.json()
+
+                        .then (data => {
+                            data.daily.forEach((valor) => {
+                                const temperaturaMax = valor.temp.max
+                                const temperaturaMin = valor.temp.min
+    
+                                tempMax.innerHTML = `${temperaturaMax}º`
+                                tempMin.innerHTML = `${temperaturaMin}º`
+
+                                //console.log(`Temperatura  Máxima: ${temperaturaMax}`)
+                                //console.log(`Temperatura Mínima: ${temperaturaMin}`)
+                            })
+                            /*data.daily.forEach((elemento) => {
+
+                                let umidade = elemento.humidity
+                                //console.log("umidade:", umidade)
+                                precipitacao.innerHTML = umidade + "%"
+                                
+                            })*/
+
+                            //Duas tentativas diferentes de pegar a umidade
+                            
+                            for (let h = 0; h < 7; h++ ){
+                                let umidade = data['daily'][h]['humidity']
+                                precipitacao.innerHTML[h] = umidade
+                                console.log(umidade)
+                            }
+                            for (let i = 0; i < 7 ; i++){
+                                let sensacao = data['daily'][i]
+                                console.log(sensacao['feels_like'])
+
+                                if (moment == 'day'){
+                                    sensation.innerHTML = `Sensação térmica: ${sensacao['feels_like']['day']}`
+                                } else if(moment == 'morn'){
+                                    sensation.innerHTML = `Sensação térmica: ${sensacao['feels_like']['morn']}`
+                                } else if (moment == 'eve'){
+                                    sensation.innerHTML = `Sensação térmica: ${sensacao['feels_like']['eve']}`
+                                }
+                            }
+                        })
+                    })
+                    .catch (error =>{
+                        console.log("Erro, reinicie a página e tente novamente")
+                    })
+            })
+        })
+
+        .catch(e => {
+            console.log('Erro, reinicie a página e tente novamente')
+        })
+    
+}
